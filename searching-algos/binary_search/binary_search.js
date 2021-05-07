fedd57dd803f1d643ac3d3921fe4aa42740e7056
gabriel-demes <Gabrieldemes@nyu.edu>
2021-05-07 13:00:11 -0400

    binary search reccursive

--- searching-algos/binary_search/binary_search.js
+++ searching-algos/binary_search/binary_search.js
@@ -1,21 +1,36 @@
-const binary_search = (array, value) => {
-    let left = array[0]
-    let right = array.length-1
+// const binary_search = (array, value) => {
+//     let left = array[0]
+//     let right = array.length-1
 
-    while(left <= right){
-        middle = Math.floor((left+right)/2)
+//     while(left <= right){
+//         middle = Math.floor((left+right)/2)
+//         if(value === array[middle]){
+//             return middle
+//         }else if(value < array[middle]){
+//             right = middle - 1
+//         }else if(value > array[middle]){
+//             left = middle + 1
+//         }
+//     }
+//     return -1
+// }
+
+const binary_search = (array, value, left, right) => {
+    let middle = Math.floor((left + right)/2)
+    if(left > right){
+        return -1
+    }else{
         if(value === array[middle]){
             return middle
-        }else if(value < array[middle]){
-            right = middle - 1
         }else if(value > array[middle]){
-            left = middle + 1
+            return binary_search(array, value, middle +1, right)
+        }else if (value < array[middle]){
+            return binary_search(array, value, left, middle - 1)
         }
     }
-    return -1
 }
 
 const array= [1,2,3,4,5,6,7,8,9]
 let num = window.prompt("Enter a value to be found")
 let n = parseInt(num)
-console.log(binary_search(array, n))
+console.log(binary_search(array, n, array[0], array.length -1))
--- searching-algos/binary_search/binary_search.py
+++ searching-algos/binary_search/binary_search.py
@@ -5,20 +5,36 @@
 # O(logN) time complexity
 import math 
 
-def binary_search(array, value):
-    left = 0
-    right = len(array) - 1
+#iteratve
+# def binary_search(array, value):
+#     left = 0
+#     right = len(array) - 1
     
-    while left <= right:
-        middle = math.floor((left + right)/2)
+#     while left <= right:
+#         middle = math.floor((left + right)/2)
+#         if value == array[middle]:
+#             return middle
+#         elif value < array[middle]:
+#             right = middle - 1
+#         elif value > array[middle]:
+#             left = middle + 1
+#     return -1
+
+#recurrsion 
+    #check to see if left is greater than right -base case to rerturn 
+def binary_search(array, value, left, right):
+    middle = math.floor((left + right)/2)
+
+    if left > right:
+        return -1
+    else:
         if value == array[middle]:
             return middle
-        elif value < array[middle]:
-            right = middle - 1
         elif value > array[middle]:
-            left = middle + 1
-    return -1
+            return binary_search(array, value, middle + 1, right)
+        elif value < array[middle]:
+            return binary_search(array, value, left, middle -1)
 
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 num = int(input("Enter a value to find "))
-print(binary_search(array, num))
+print(binary_search(array, num, array[0], len(array)-1))
