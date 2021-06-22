35ba2104dfc18beb4f1f8205e4aaaa6e054659fb
gabriel-demes <Gabrieldemes@nyu.edu>
2021-06-21 23:16:11 -0400

    tree traversal depth first search preOrder

---
+++ trees/tree-traversal/DFS-preorder.js
@@ -0,0 +1,84 @@
+class Node{
+    constructor(val){
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+class BST{
+    constructor(val){
+        this.root = null
+    }
+
+    insert(val){
+        let node = new Node(val)
+        if(!this.root){
+            this.root = node
+            return this
+        }
+        let cur = this.root
+        while(true){
+            if(val === cur.val) return undefined
+            if(val < cur.val){
+                if(!cur.left){
+                    cur.left = node
+                    return this
+                }
+                cur = cur.left
+            }else{
+                if(!cur.right){
+                    cur.right = node
+                    return this
+                }
+                cur = cur.right
+            }
+        }
+        return this
+    }
+
+    find(val){
+        if(!this.root) return false
+        let cur = this.root
+        let found = false 
+
+        while(!found && cur){
+            if(val < cur.val){
+                cur = cur.left;
+            }else if(val > cur.val){
+                cur = cur.right
+            }else{
+                found = true
+            }
+        }
+        return found ? cur : undefined
+    }
+
+}
+
+const tree = new BST()
+tree.insert(10)
+tree.insert(5)
+tree.insert(2)
+tree.insert(6)
+tree.insert(4)
+tree.insert(13)
+
+
+const dfsPreOrder = (tree) => {
+    let arr = []
+    let cur = tree.root
+    const traverse = (node) => {
+        arr.push(node.val)
+        if(node.left){
+            traverse(node.left)
+        }
+        if(node.right){
+            traverse(node.right)
+        }
+    }
+    traverse(cur)
+
+    return arr
+}
+
+console.log(dfsPreOrder(tree))
